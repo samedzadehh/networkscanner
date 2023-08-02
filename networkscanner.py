24e0a5a8824865c462d96e6f2a512321e0308b92
Huseyn Samedzade <129556088+samedzadehh@users.noreply.github.com>
2023-08-03 03:54:02 +0400

    networkscanner.py

--- networkscanner.py
+++ networkscanner.py
@@ -4,7 +4,7 @@ def scan_ports(target_ip,start_port,end_port):
     for port in range(start_port,end_port+1):
         try:
             sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            sock.settimeout(0.00001)
+            sock.settimeout(1)
             result=sock.connect_ex((target_ip,port))
             if result==0:
                 open_ports.append(port)
